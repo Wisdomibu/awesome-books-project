0d6866a7c525961261706bad33c231401f1cbf6a
Wisdom Ibu <wisdomibu@gmail.com>
2023-09-12 19:53:34 +0100

    Styled the page

--- script.js
+++ script.js
@@ -1,60 +1,63 @@
 class BookList {
   constructor() {
-    this.textWrapper = document.querySelector(".textWrapper");
-    this.addBtn = document.querySelector("#btn");
-    this.input = document.querySelectorAll(".input");
-    this.bookList = JSON.parse(localStorage.getItem("myBookList")) || [];
-    this.addBtn.addEventListener("click", this.addBook.bind(this));
+    this.textWrapper = document.querySelector('.textWrapper');
+    this.addBtn = document.querySelector('#btn');
+    this.input = document.querySelectorAll('.input');
+    this.bookList = JSON.parse(localStorage.getItem('myBookList')) || [];
+    this.addBtn.addEventListener('click', this.addBook.bind(this));
     this.renderBooks();
   }
 
   renderBooks() {
-    this.textWrapper.innerHTML = "";
+    this.textWrapper.innerHTML = '';
     this.bookList.forEach((book) => {
       this.createElem(book.title, book.author);
     });
   }
 
   createElem(inputValueTitle, inputValueAuthor) {
-    const container = document.createElement("div");
-    container.className = "bookContainer";
-    const h3 = document.createElement("h3");
+    const container = document.createElement('div');
+    container.className = 'bookContainer';
+    const h3 = document.createElement('h3');
     h3.innerHTML = inputValueTitle;
-    const p = document.createElement("p");
-    p.innerText = inputValueAuthor;
-    const button = document.createElement("button");
-    button.innerText = "Remove";
-    const divLine = document.createElement("div");
-    divLine.className = "line";
+    const p = document.createElement('p');
+    p.innerText = `by ${inputValueAuthor}  `;
+    const listDiv = document.createElement('div');
+    listDiv.append(h3, p);
+    listDiv.className = 'listdiv';
 
-    button.addEventListener("click", () => {
-      container.style.display = "none";
+    const button = document.createElement('button');
+    button.innerText = 'Remove';
+
+    button.addEventListener('click', () => {
+      container.style.display = 'none';
       this.removeBook(inputValueTitle, inputValueAuthor);
     });
 
-    container.append(h3, p, button, divLine);
+    container.append(listDiv, button);
     this.textWrapper.appendChild(container);
   }
 
   addBook() {
     const inputValueTitle = this.input[0].value;
     const inputValueAuthor = this.input[1].value;
-    if (inputValueTitle !== "" && inputValueAuthor !== "") {
+    if (inputValueTitle !== '' && inputValueAuthor !== '') {
       this.bookList.push({ title: inputValueTitle, author: inputValueAuthor });
       this.createElem(inputValueTitle, inputValueAuthor);
-      localStorage.setItem("myBookList", JSON.stringify(this.bookList));
+      localStorage.setItem('myBookList', JSON.stringify(this.bookList));
     }
-    this.input[0].value = "";
-    this.input[1].value = "";
+
+    this.input[0].value = '';
+    this.input[1].value = '';
   }
 
   removeBook(title, author) {
     const bookIndex = this.bookList.findIndex(
-      (book) => book.title === title && book.author === author
+      (book) => book.title === title && book.author === author,
     );
     if (bookIndex !== -1) {
       this.bookList.splice(bookIndex, 1);
-      localStorage.setItem("myBookList", JSON.stringify(this.bookList));
+      localStorage.setItem('myBookList', JSON.stringify(this.bookList));
     }
   }
 }
