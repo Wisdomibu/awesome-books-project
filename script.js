4433a4db1b2e782f0949278e7e456263dac1f2ff
jimoh oluwaseyi <heysey128@gmail.com>
2023-09-14 12:49:57 +0100

    Modify the file

--- script.js
+++ script.js
@@ -1,65 +1,62 @@
+const textWrapper = document.querySelector(".textWrapper");
+const addBtn = document.querySelector("#btn");
+const input = document.querySelectorAll(".input");
+
 class BookList {
   constructor() {
-    this.textWrapper = document.querySelector('.textWrapper');
-    this.addBtn = document.querySelector('#btn');
-    this.input = document.querySelectorAll('.input');
-    this.bookList = JSON.parse(localStorage.getItem('myBookList')) || [];
-    this.addBtn.addEventListener('click', this.addBook.bind(this));
-    this.renderBooks();
+    this.addBook = this.addBook.bind(this);
+    this.createElem = this.createElem.bind(this);
+    addBtn.addEventListener("click", this.addBook);
   }
 
-  renderBooks() {
-    this.textWrapper.innerHTML = '';
+  loadBooks() {
+    this.bookList = JSON.parse(localStorage.getItem("myBookList")) || [];
     this.bookList.forEach((book) => {
       this.createElem(book.title, book.author);
     });
   }
 
-  createElem(inputValueTitle, inputValueAuthor) {
-    const container = document.createElement('div');
-    container.className = 'bookContainer';
-    const h3 = document.createElement('h3');
+  createElem(inputValueTitle, inputValueaAuthor) {
+    const container = document.createElement("div");
+    container.className = "bookContainer";
+    const h3 = document.createElement("h3");
     h3.innerHTML = inputValueTitle;
-    const p = document.createElement('p');
-    p.innerText = `by ${inputValueAuthor}  `;
-    const listDiv = document.createElement('div');
-    listDiv.append(h3, p);
-    listDiv.className = 'listdiv';
-
-    const button = document.createElement('button');
-    button.innerText = 'Remove';
-
-    button.addEventListener('click', () => {
-      container.style.display = 'none';
-      this.removeBook(inputValueTitle, inputValueAuthor);
+    const p = document.createElement("p");
+    p.innerText = `by ${inputValueaAuthor}`;
+    const button = document.createElement("button");
+    button.innerText = "Remove";
+    const divLine = document.createElement("div");
+    divLine.append(h3, p);
+    divLine.className = "listdiv";
+    // Remove Button part
+    button.addEventListener("click", () => {
+      container.style.display = "none";
+      this.bookList.splice(
+        this.bookList.findIndex(
+          (book) =>
+            book.title === inputValueTitle && book.author === inputValueaAuthor
+        ),
+        1
+      );
+      localStorage.setItem("myBookList", JSON.stringify(this.bookList));
     });
 
-    container.append(listDiv, button);
-    this.textWrapper.appendChild(container);
+    container.append(divLine, button);
+    textWrapper.appendChild(container);
   }
 
   addBook() {
-    const inputValueTitle = this.input[0].value;
-    const inputValueAuthor = this.input[1].value;
-    if (inputValueTitle !== '' && inputValueAuthor !== '') {
-      this.bookList.push({ title: inputValueTitle, author: inputValueAuthor });
-      this.createElem(inputValueTitle, inputValueAuthor);
-      localStorage.setItem('myBookList', JSON.stringify(this.bookList));
+    const inputValueTitle = input[0].value;
+    const inputValueaAuthor = input[1].value;
+    if (inputValueTitle !== "" && inputValueaAuthor !== "") {
+      this.bookList.push({ title: inputValueTitle, author: inputValueaAuthor });
+      this.createElem(inputValueTitle, inputValueaAuthor);
+      localStorage.setItem("myBookList", JSON.stringify(this.bookList));
     }
-
-    this.input[0].value = '';
-    this.input[1].value = '';
-  }
-
-  removeBook(title, author) {
-    const bookIndex = this.bookList.findIndex(
-      (book) => book.title === title && book.author === author,
-    );
-    if (bookIndex !== -1) {
-      this.bookList.splice(bookIndex, 1);
-      localStorage.setItem('myBookList', JSON.stringify(this.bookList));
-    }
+    input[0].value = "";
+    input[1].value = "";
   }
 }
 
-const bookListApp = new BookList();
+const books = new BookList();
+books.loadBooks();
